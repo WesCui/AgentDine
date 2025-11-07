46a0592af96a093cf087903bec8e70292157adfa
WesCui <1031563986@qq.com>
2025-11-07 21:51:47 +0800

    新增菜品业务代码

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,5 +1,6 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
 import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
@@ -46,7 +47,7 @@ public class CommonController {
         }
 
 
-        return null;
+        return Result.error(MessageConstant.UPLOAD_FAILED);
     }
 
 
